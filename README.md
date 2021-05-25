6cbf2b835d67a7c974a0501ca8228248422b044e
Ryan Maas <rdmjobs@live.com>
2021-05-25 17:36:43 -0500

    Added POST /artist DB functionality successfully.

--- README.md
+++ README.md
@@ -39,8 +39,8 @@ Using the ERD above:
 ### Update the Server-side Code
 
 - [x] Install `pg` and setup your `pool` to connect to the database.
-- [ ] Update the `\artist` GET route to get data from the database. Sort the data by birthdate, youngest to oldest.
-- [ ] Update the `\artist` POST route to store data in the database. 
+- [x] Update the `\artist` GET route to get data from the database. Sort the data by birthdate, youngest to oldest.
+- [x] Update the `\artist` POST route to store data in the database. 
 - [ ] Update the `\song` GET route to get data from the database. Sort the data by title.
 - [ ] Update the `\song` POST route to store data in the database.  
 
--- server/server.js
+++ server/server.js
@@ -25,10 +25,9 @@ pool.on( 'error', error => {
 });
 //#endregion ⬆⬆ Server functionality setup setup above. 
 
-// TODO - Replace static content with a database tables
-
 
 //#region ⬇⬇ All GET & POST routes below: 
+// ⬇ GET /artist route to DB:
 app.get('/artist', (req, res) => {
   console.log(`In /artist GET`);
   // ⬇ Declare variable for SQL queries:
@@ -44,57 +43,33 @@ app.get('/artist', (req, res) => {
   }); // End pool.query call. 
 }); // End GET /artist route. 
 
+// ⬇ POST /artist route to DB: 
 app.post('/artist', (req, res) => {
-    artistList.push(req.body);
-    res.sendStatus(201);
-});
+  // ⬇ Declare variable for SQL queries:
+  let queryText = `INSERT INTO "artist" ("name", "birthdate")
+  VALUES ($1, $2);`
+  // ⬇ Sanitize your inputs:
+  let values = [req.body.name, req.body.birthdate];
+  // ⬇ Setup pool.query to communicate:
+  pool.query( queryText, values )
+  .then( result => {
+    console.log( result.rows );
+    res.sendStatus( 201 ); // CREATED. 
+  }).catch( error => {
+    console.log( error );
+    res.sendStatus( 500 ); // SERVER ERROR. 
+  }); // End pool.query call.
+}); // End POST /artist route. 
 
+// ⬇ GET /song route to DB: 
 app.get('/song', (req, res) => {
     console.log(`In /songs GET`);
     res.send(songList);
 });
 
+// ⬇ POST /song route to DB:
 app.post('/song', (req, res) => {
     songList.push(req.body);
     res.sendStatus(201);
 });
 //#endregion ⬆⬆ All GET & POST routes above. 
-
-
-//#region ⬇⬇ Old code below: 
-// const artistList = [ 
-//     {
-//         name: 'Ella Fitzgerald',
-//         birthdate: '04-25-1917'
-//     },
-//     {
-//         name: 'Dave Brubeck',
-//         birthdate: '12-06-1920'
-//     },       
-//     {
-//         name: 'Miles Davis',
-//         birthdate: '05-26-1926'
-//     },
-//     {
-//         name: 'Esperanza Spalding',
-//         birthdate: '10-18-1984'
-//     },
-// ]
-// const songList = [
-//     {
-//         title: 'Take Five',
-//         length: '5:24',
-//         released: '1959-09-29'
-//     },
-//     {
-//         title: 'So What',
-//         length: '9:22',
-//         released: '1959-08-17'
-//     },
-//     {
-//         title: 'Black Gold',
-//         length: '5:17',
-//         released: '2012-02-01'
-//     }
-// ];
-//#endregion ⬆⬆ Old code above. 
