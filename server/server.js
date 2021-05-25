bad22a0d8671162a6e5bdf6ca406c8382a27a21b
Ryan Maas <rdmjobs@live.com>
2021-05-25 17:26:26 -0500

    Added GET /artist DB functionality successfully.

--- server/server.js
+++ server/server.js
@@ -26,46 +26,23 @@ pool.on( 'error', error => {
 //#endregion ⬆⬆ Server functionality setup setup above. 
 
 // TODO - Replace static content with a database tables
-const artistList = [ 
-    {
-        name: 'Ella Fitzgerald',
-        birthdate: '04-25-1917'
-    },
-    {
-        name: 'Dave Brubeck',
-        birthdate: '12-06-1920'
-    },       
-    {
-        name: 'Miles Davis',
-        birthdate: '05-26-1926'
-    },
-    {
-        name: 'Esperanza Spalding',
-        birthdate: '10-18-1984'
-    },
-]
-const songList = [
-    {
-        title: 'Take Five',
-        length: '5:24',
-        released: '1959-09-29'
-    },
-    {
-        title: 'So What',
-        length: '9:22',
-        released: '1959-08-17'
-    },
-    {
-        title: 'Black Gold',
-        length: '5:17',
-        released: '2012-02-01'
-    }
-];
 
+
+//#region ⬇⬇ All GET & POST routes below: 
 app.get('/artist', (req, res) => {
-    console.log(`In /songs GET`);
-    res.send(artistList);
-});
+  console.log(`In /artist GET`);
+  // ⬇ Declare variable for SQL queries:
+  const queryText = `SELECT * FROM "artist" ORDER BY "birthdate" DESC;`;
+  // ⬇ Setup pool.query to communicate:
+  pool.query( queryText )
+  .then( result => {
+    console.log( result.rows ); // Have to narrow down our data? 
+    res.send(  result.rows );
+  }).catch( error => { // If pool.query fails:
+    console.log( error );
+    res.sendStatus( 500 ); // Server error, not client error. 
+  }); // End pool.query call. 
+}); // End GET /artist route. 
 
 app.post('/artist', (req, res) => {
     artistList.push(req.body);
@@ -81,5 +58,43 @@ app.post('/song', (req, res) => {
     songList.push(req.body);
     res.sendStatus(201);
 });
+//#endregion ⬆⬆ All GET & POST routes above. 
 
 
+//#region ⬇⬇ Old code below: 
+// const artistList = [ 
+//     {
+//         name: 'Ella Fitzgerald',
+//         birthdate: '04-25-1917'
+//     },
+//     {
+//         name: 'Dave Brubeck',
+//         birthdate: '12-06-1920'
+//     },       
+//     {
+//         name: 'Miles Davis',
+//         birthdate: '05-26-1926'
+//     },
+//     {
+//         name: 'Esperanza Spalding',
+//         birthdate: '10-18-1984'
+//     },
+// ]
+// const songList = [
+//     {
+//         title: 'Take Five',
+//         length: '5:24',
+//         released: '1959-09-29'
+//     },
+//     {
+//         title: 'So What',
+//         length: '9:22',
+//         released: '1959-08-17'
+//     },
+//     {
+//         title: 'Black Gold',
+//         length: '5:17',
+//         released: '2012-02-01'
+//     }
+// ];
+//#endregion ⬆⬆ Old code above. 
